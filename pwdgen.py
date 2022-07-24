40f725b65035089ce80555b7de0ee90ed507a85c
karankumarftw <109886486+karankumarftw@users.noreply.github.com>
2022-07-24 11:10:09 +0530

    Uploaded

---
+++ pwdgen.py
@@ -0,0 +1,365 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtPrintSupport import *
+import sys,sqlite3,time
+import random
+import array
+
+import os
+
+class InsertDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(InsertDialog, self).__init__(*args, **kwargs)
+
+        self.QBtn = QPushButton()
+        self.QBtn.setText("Generate")
+
+        self.setWindowTitle("New Password")
+        self.setFixedWidth(300)
+        self.setFixedHeight(250)
+
+        self.QBtn.clicked.connect(self.additem)
+
+        layout = QVBoxLayout()
+
+        self.nameinput = QLineEdit()
+        self.nameinput.setPlaceholderText("UserName")
+        layout.addWidget(self.nameinput)
+
+        self.branchinput = QComboBox()
+        self.branchinput.addItem("Facebook")
+        self.branchinput.addItem("Twitter")
+        self.branchinput.addItem("Instagram")
+        self.branchinput.addItem("Google")
+        self.branchinput.addItem("Vimeo")
+        layout.addWidget(self.branchinput)
+
+        
+
+       
+        layout.addWidget(self.QBtn)
+        self.setLayout(layout)
+
+    def additem(self):
+        MAX_LEN = 12
+
+
+        DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z']
+
+        UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
+
+        SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>','*', '(', ')', '<']
+
+
+        COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+
+
+        rand_digit = random.choice(DIGITS)
+        rand_upper = random.choice(UPCASE_CHARACTERS)
+        rand_lower = random.choice(LOCASE_CHARACTERS)
+        rand_symbol = random.choice(SYMBOLS)
+
+
+        temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
+
+
+
+        for x in range(MAX_LEN - 4):
+            temp_pass = temp_pass + random.choice(COMBINED_LIST)
+
+    
+            temp_pass_list = array.array('u', temp_pass)
+            random.shuffle(temp_pass_list)
+
+
+        password = ""
+        for x in temp_pass_list:
+            password = password + x
+
+
+        name = ""
+        branch = ""
+        
+
+        name = self.nameinput.text()
+        branch = self.branchinput.itemText(self.branchinput.currentIndex())
+        
+        
+        
+        self.conn = sqlite3.connect("database.db")
+        self.c = self.conn.cursor()
+        self.c.execute("INSERT INTO karan (username,sm,password) VALUES (?,?,?)",(name,branch,password))
+        self.conn.commit()
+        self.c.close()
+        self.conn.close()
+        QMessageBox.information(QMessageBox(),'Successful','Password  generated successfully to the database.')
+        self.close()
+        
+
+class SearchDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(SearchDialog, self).__init__(*args, **kwargs)
+
+        self.QBtn = QPushButton()
+        self.QBtn.setText("Search")
+
+        self.setWindowTitle("Find")
+        self.setFixedWidth(300)
+        self.setFixedHeight(100)
+        self.QBtn.clicked.connect(self.searchitem)
+        layout = QVBoxLayout()
+
+        self.searchinput = QLineEdit()
+        self.onlyInt = QIntValidator()
+        self.searchinput.setValidator(self.onlyInt)
+        self.searchinput.setPlaceholderText("Id")
+        layout.addWidget(self.searchinput)
+        layout.addWidget(self.QBtn)
+        self.setLayout(layout)
+
+    def searchitem(self):
+
+        searchrol = ""
+        searchrol = self.searchinput.text()
+        try:
+            self.conn = sqlite3.connect("database.db")
+            self.c = self.conn.cursor()
+            result = self.c.execute("SELECT * from karan WHERE id="+str(searchrol))
+            row = result.fetchone()
+            searchresult = "Id : "+str(row[0])+'\n'+"UserName : "+str(row[1])+'\n'+"SocialMedia : "+str(row[2])+'\n'+"Password : "+str(row[3])
+            QMessageBox.information(QMessageBox(), 'Successful', searchresult)
+            self.conn.commit()
+            self.c.close()
+            self.conn.close()
+        except Exception:
+            QMessageBox.warning(QMessageBox(), 'Error', 'Could not Find item from the database.')
+
+class DeleteDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(DeleteDialog, self).__init__(*args, **kwargs)
+
+        self.QBtn = QPushButton()
+        self.QBtn.setText("Delete")
+
+        self.setWindowTitle("Delete Item")
+        self.setFixedWidth(300)
+        self.setFixedHeight(100)
+        self.QBtn.clicked.connect(self.deletestudent)
+        layout = QVBoxLayout()
+
+        self.deleteinput = QLineEdit()
+        self.onlyInt = QIntValidator()
+        self.deleteinput.setValidator(self.onlyInt)
+        self.deleteinput.setPlaceholderText("Id")
+        layout.addWidget(self.deleteinput)
+        layout.addWidget(self.QBtn)
+        self.setLayout(layout)
+
+    def deletestudent(self):
+
+        delrol = ""
+        delrol = self.deleteinput.text()
+        try:
+            self.conn = sqlite3.connect("database.db")
+            self.c = self.conn.cursor()
+            self.c.execute("DELETE from karan WHERE id="+str(delrol))
+            self.conn.commit()
+            self.c.close()
+            self.conn.close()
+            QMessageBox.information(QMessageBox(),'Successful','Deleted From Table Successful')
+            self.close()
+        except Exception:
+            QMessageBox.warning(QMessageBox(), 'Error', 'Could not Delete item from the database.')
+
+class LoginDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(LoginDialog, self).__init__(*args, **kwargs)
+
+        self.setFixedWidth(300)
+        self.setFixedHeight(120)
+
+        layout = QVBoxLayout()
+
+        self.passinput = QLineEdit()
+        self.passinput.setEchoMode(QLineEdit.Password)
+        self.passinput.setPlaceholderText("Enter Password.")
+        self.QBtn = QPushButton()
+        self.QBtn.setText("Login")
+        self.setWindowTitle('Login')
+        self.QBtn.clicked.connect(self.login)
+
+        title = QLabel("Login")
+        font = title.font()
+        font.setPointSize(16)
+        title.setFont(font)
+
+        layout.addWidget(title)
+        layout.addWidget(self.passinput)
+        layout.addWidget(self.QBtn)
+        self.setLayout(layout)
+
+    def login(self):
+        if(self.passinput.text() == "kkftw"):
+            self.accept()
+        else:
+            QMessageBox.warning(self, 'Error', 'Wrong Password')
+
+
+
+
+
+class AboutDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(AboutDialog, self).__init__(*args, **kwargs)
+
+        self.setFixedWidth(300)
+        self.setFixedHeight(175)
+
+        QBtn = QDialogButtonBox.Ok 
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        layout = QVBoxLayout()
+        self.setWindowTitle('About')
+        title = QLabel("Password Generator")
+        font = title.font()
+        font.setPointSize(12)
+        title.setFont(font)
+
+        labelpic = QLabel()
+        
+        labelpic.setFixedHeight(150)
+
+        layout.addWidget(title)
+
+        layout.addWidget(QLabel("Developed By karankumar"))
+        layout.addWidget(labelpic)
+
+
+        layout.addWidget(self.buttonBox)
+
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.conn = sqlite3.connect("database.db")
+        self.c = self.conn.cursor()
+        
+        self.c.close()
+
+        file_menu = self.menuBar().addMenu("&File")
+
+        help_menu = self.menuBar().addMenu("&About")
+        self.setWindowTitle("Password Generator")
+
+        self.setMinimumSize(800, 600)
+
+        self.tableWidget = QTableWidget()
+        self.setCentralWidget(self.tableWidget)
+        self.tableWidget.setAlternatingRowColors(True)
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.horizontalHeader().setCascadingSectionResizes(False)
+        self.tableWidget.horizontalHeader().setSortIndicatorShown(False)
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.tableWidget.verticalHeader().setCascadingSectionResizes(False)
+        self.tableWidget.verticalHeader().setStretchLastSection(False)
+        self.tableWidget.setHorizontalHeaderLabels(("id", "UserName", "SocialMedia", "Password"))
+
+        toolbar = QToolBar()
+        toolbar.setMovable(False)
+        self.addToolBar(toolbar)
+
+        statusbar = QStatusBar()
+        self.setStatusBar(statusbar)
+
+        btn_ac_adduser = QAction(QIcon("icon/add.png"), "New Generate", self)
+        btn_ac_adduser.triggered.connect(self.insert)
+        btn_ac_adduser.setStatusTip("Add Student")
+        toolbar.addAction(btn_ac_adduser)
+
+        btn_ac_refresh = QAction(QIcon("icon/refresh.png"),"Refresh",self)
+        btn_ac_refresh.triggered.connect(self.loaddata)
+        btn_ac_refresh.setStatusTip("Refresh Table")
+        toolbar.addAction(btn_ac_refresh)
+
+        btn_ac_search = QAction(QIcon("icon/search.png"), "Search", self)
+        btn_ac_search.triggered.connect(self.search)
+        btn_ac_search.setStatusTip("Search User")
+        toolbar.addAction(btn_ac_search)
+
+        btn_ac_delete = QAction(QIcon("icon/trash.png"), "Delete", self)
+        btn_ac_delete.triggered.connect(self.delete)
+        btn_ac_delete.setStatusTip("Delete User")
+        toolbar.addAction(btn_ac_delete)
+
+        adduser_action = QAction(QIcon("icon/add.png"),"New Generate", self)
+        adduser_action.triggered.connect(self.insert)
+        file_menu.addAction(adduser_action)
+
+        searchuser_action = QAction(QIcon("icon/search.png"), "Search", self)
+        searchuser_action.triggered.connect(self.search)
+        file_menu.addAction(searchuser_action)
+
+        deluser_action = QAction(QIcon("icon/trash.png"), "Delete", self)
+        deluser_action.triggered.connect(self.delete)
+        file_menu.addAction(deluser_action)
+
+
+        about_action = QAction(QIcon("icon/info.png"),"Developer", self)
+        about_action.triggered.connect(self.about)
+        help_menu.addAction(about_action)
+
+    def loaddata(self):
+        self.connection = sqlite3.connect("database.db")
+        query = "SELECT * FROM karan"
+        result = self.connection.execute(query)
+        self.tableWidget.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.tableWidget.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.tableWidget.setItem(row_number, column_number,QTableWidgetItem(str(data)))
+        self.connection.close()
+
+    def handlePaintRequest(self, printer):
+        document = QTextDocument()
+        cursor = QTextCursor(document)
+        model = self.table.model()
+        table = cursor.insertTable(
+            model.rowCount(), model.columnCount())
+        for row in range(table.rows()):
+            for column in range(table.columns()):
+                cursor.insertText(model.item(row, column).text())
+                cursor.movePosition(QTextCursor.NextCell)
+        document.print_(printer)
+
+    def insert(self):
+        dlg = InsertDialog()
+        dlg.exec_()
+
+    def delete(self):
+        dlg = DeleteDialog()
+        dlg.exec_()
+
+    def search(self):
+        dlg = SearchDialog()
+        dlg.exec_()
+
+    def about(self):
+        dlg = AboutDialog()
+        dlg.exec_()
+
+
+app = QApplication(sys.argv)
+passdlg = LoginDialog()
+if(passdlg.exec_() == QDialog.Accepted):
+    window = MainWindow()
+    window.show()
+    window.loaddata()
+sys.exit(app.exec_())
